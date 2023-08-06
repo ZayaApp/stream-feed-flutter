f92d5114e3b31724c83073a2a3145f3885147797
Khainarbyek <53472764+Khainarbyek@users.noreply.github.com>
2023-08-06 15:41:08 +0800

    Update open_graph_data.g.dart
    og scrape width & height is int

--- packages/stream_feed/lib/src/core/models/open_graph_data.g.dart
+++ packages/stream_feed/lib/src/core/models/open_graph_data.g.dart
@@ -45,8 +45,8 @@ OgImage _$OgImageFromJson(Map json) => OgImage(
       image: json['image'] as String?,
       url: json['url'] as String?,
       secureUrl: json['secure_url'] as String?,
-      width: json['width'] as String?,
-      height: json['height'] as String?,
+      width: (json['width'] is int) ? "${json['width']}" : json["width"] as String?;
+      height: (json['height'] is int) ? "${json['height']}" : json["height"] as String?;
       type: json['type'] as String?,
       alt: json['alt'] as String?,
     );
